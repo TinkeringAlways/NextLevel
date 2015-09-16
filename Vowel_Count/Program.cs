6c34a2a610a0e22b52204aea760542e567bb0fac
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-16 20:42:37 +0000

    Finished Vowel_Count!!!

---
+++ Vowel_Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("abrecidbra"));
+            Console.ReadLine();
+        }
+
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            char[] temp = str.ToCharArray();
+
+            foreach (char c in temp)
+            {
+                if (new [] {'a','e','i','o','u'}.Contains(c))
+                {
+                    vowelCount++;
+                }
+            }
+
+
+            return vowelCount;
+        }
+    }
+}
