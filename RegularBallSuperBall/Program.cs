9e123c13263c9eadbbaa5ae8657750fc4b173e58
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 17:21:27 +0000

    Can't figure out this stupid ball!!

---
+++ RegularBallSuperBall/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularBallSuperBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball b = new Ball();
+            Ball sB = new Ball("super");
+
+            Console.WriteLine(b.balltype);
+            Console.WriteLine(sB.balltype);
+            Console.ReadLine();
+        }
+    }
+
+    public class Ball
+    {
+        public string balltype { get; set; }
+
+        public Ball()
+        {
+            balltype = "regular";
+        }
+
+        public Ball(string type = "super")
+        {
+            balltype = type;
+        }
+    }
+}
