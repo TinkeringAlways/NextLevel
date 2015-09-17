071f4991af5ee5ba658f2c6a3841649219dc3626
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 18:02:15 +0000

    Go back to Vasya and Stairs and finish it. Need to get the math!!

---
+++ VasyaAndStairs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VasyaAndStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfSteps(100,7));
+            Console.ReadLine();
+        }
+
+        public static int NumberOfSteps(int n, int m)
+        {
+            int howMany = 0;
+
+            if (n < m)
+            {
+                howMany = -1;
+            }
+            else
+            {
+                int ones = 0;
+
+                if ((n / 2) % m != 0)
+                {
+                    int rem = (n/m) % m;
+
+                }
+                else
+                {
+                    howMany = (n/m);
+                }
+            }
+
+            return howMany;
+        }
+    }
+}
