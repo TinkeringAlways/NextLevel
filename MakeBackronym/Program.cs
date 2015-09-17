e09646db26cb7320229bb4244ed25cc3959b37c6
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 14:53:18 +0000

    MakeBackronym....... I can't find where to load this DICTIONARY from!! Ahhh

---
+++ MakeBackronym/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeBackronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static string MakeBackronym(string s)
+        {
+            string input = s.ToUpper();
+            char[] c = input.ToCharArray();
+            string acro = String.Empty;
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                acro += c[0] + " ";
+            }
+
+            string final = acro.Trim();
+
+            return final; // "awesome"
+        }
+    }
+}
