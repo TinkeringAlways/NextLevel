b5dd8aef1074a058032b9c3cf14e172cc347c874
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 13:41:26 +0000

    Just completed sum of the first nth! pretty cool, not the most efficient code

---
+++ SumOfTheFirstNthTermOfSeries/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTheFirstNthTermOfSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(seriesSum(5));
+            Console.ReadLine();
+        }
+
+        public static string seriesSum(int n)
+        {
+            double num;
+
+            if (n == 1)
+            {
+                num = 1;
+            }
+            else if (n == 2)
+            {
+                num = 1 + (1/4d);
+            }
+            else if (n >= 3)
+            {
+                double denom;
+                num = 1 + (1/4d);
+
+                for (int i = 1; i <= n - 2; i++)
+                {
+                    denom = 4 + (i*3);
+                    num = num +  (1/denom);
+                }
+            }
+            else
+            {
+                num = 0;
+            }
+
+            string total = num.ToString("F2");
+
+            return total;
+        }
+    }
+}
