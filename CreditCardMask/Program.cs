c454ace8594b2462c5faff0ae8c163036bd7f2a2
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 18:49:52 +0000

    Completed Maskify!!

---
+++ CreditCardMask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardMask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maskify("78987654567898765"));
+            Console.ReadLine();
+        }
+
+        public static string Maskify(string cc)
+        {
+            if (cc.Length > 4)
+            {
+                string temp = cc.Substring(0, (cc.Length - 4));
+                string mask = String.Empty;
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    mask += "#";
+                }
+
+                cc = cc.Remove(0, (temp.Length));
+                cc = mask + cc;
+            }
+            
+            return cc;
+        }
+    }
+}
