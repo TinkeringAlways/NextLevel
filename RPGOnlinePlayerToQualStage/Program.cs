1f73c528fd60a630534c9c5ea46508d7ae4fd6ca
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 14:22:47 +0000

    Completed easy RPG if/else type method to chack point level

---
+++ RPGOnlinePlayerToQualStage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGOnlinePlayerToQualStage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PlayerRankUp(80));
+            Console.WriteLine(PlayerRankUp(110));
+            Console.ReadLine();
+        }
+
+        public static Object PlayerRankUp(int points)
+        {
+            if (points >= 100)
+            {
+                return
+                    "Well done!You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
+            }
+            else
+            {
+                return false;
+            }
+
+            //Better code I found after completing the task
+            //return points >= 100 ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." : (Object)false;
+        }
+    }
+}
