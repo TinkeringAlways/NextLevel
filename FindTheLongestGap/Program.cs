69ac34b966e8d704f09d806f07e143dacc59040c
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 20:48:20 +0000

    Need to finish the Longest Gap problem. Really neat how you can pass an integer into a array of bits! LOVE IT

---
+++ FindTheLongestGap/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace FindTheLongestGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gap(6789));
+            Console.ReadLine();
+        }
+
+        public static int Gap(int num)
+        {
+            int gap = 0;
+            BitArray b = new BitArray(new[] { num });
+
+            return gap;
+        }
+    }
+}
