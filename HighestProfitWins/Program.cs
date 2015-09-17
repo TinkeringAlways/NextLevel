c78ae9685371e6f7c649168e77c44aafb4264f08
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-17 19:27:03 +0000

    Done with Highest Profit Wins!! LINQ is the best!!

---
+++ HighestProfitWins/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.String;
+
+namespace HighestProfitWins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {7, 3, 7, 2, 8, 1, 5, 9, 3};
+            string lowNhigh = Join(",", new List<int>(minMax(nums)).ConvertAll(i => i.ToString()).ToArray());
+            Console.WriteLine(lowNhigh);
+            Console.ReadLine();
+        }
+
+        public static int[] minMax(int[] lst)
+        {
+            int[] lowHigh = {lst.Min(),lst.Max()};
+
+            return lowHigh;
+        }
+    }
+}
