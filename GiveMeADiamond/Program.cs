36597ee32326fa0467e6f981fb3227e4cd4f370f
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-18 18:07:03 +0000

    I love this problem. Need to draw it out more and play around. It's the Diamond Problem!! Check back soon

---
+++ GiveMeADiamond/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveMeADiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(print(7));
+            Console.ReadLine();
+        }
+
+        public static string print(int n)
+        {
+            if (n%2 == 0 && n > 0) return null;
+
+            string stars;
+
+            for (int i = 0; i < n; i++)
+            {
+                stars 
+            }
+        }
+    }
+}
