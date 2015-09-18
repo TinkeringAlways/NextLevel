f6635aa3f5674ad56b1e3b9f54c530021aa67427
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-18 15:38:58 +0000

    Just built my first extension method and also beat a KATA in BETA!!! NeatO

---
+++ FormatNumberAsCurrency/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatNumberAsCurrency
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((-1.23m).ToCurrency("#"));
+            Console.ReadLine();
+        }
+
+        public static string ToCurrency(this decimal d, string c)
+        {
+            return (d < 0) ? '-' + c + (d.ToString("F2")).TrimStart('-') : c + d.ToString("F2");
+        }
+    }
+}
