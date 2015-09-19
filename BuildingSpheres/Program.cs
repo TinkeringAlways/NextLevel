f30fec7350c4c6057c5a790951b4c9ab9caf5db9
TinkeringAlways <alwaystinkering@outlook.com>
2015-09-19 18:28:59 -0500

    Finished BuildingSpheres!! YAY, I'm CRUSHING IT

---
+++ BuildingSpheres/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingSpheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphere ball = new Sphere(2, 50);
+
+            Console.WriteLine(ball.GetRadius());
+            Console.WriteLine(ball.GetMass());
+            Console.WriteLine(ball.GetVolume());
+            Console.WriteLine(ball.GetSurfaceArea());
+            Console.WriteLine(ball.GetDensity());
+            Console.ReadLine();
+        }
+    }
+}
