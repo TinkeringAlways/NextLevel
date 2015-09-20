a2007317d9d1c154ec2a872187222ceb371607ea
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-20 22:56:20 +0000

    Just syncing

---
+++ NotCodeWars_FormatDecimalPlaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotCodeWars_FormatDecimalPlaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoundDecimal();
+            Console.ReadLine();
+        }
+
+        static void RoundDecimal()
+        {
+            decimal value = 3456787654.567851m;
+            Console.WriteLine(value);
+
+            Console.WriteLine(value.ToString("#.##"));
+            Console.WriteLine(String.Format("{0:0.00}", value));
+            Console.WriteLine($"{value:0.00}");
+            Console.WriteLine(value.ToString("n2"));
+            Console.WriteLine("{0:c}", value);
+        }
+    }
+}
