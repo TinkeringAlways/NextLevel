544f711c9c98b62bd3e83774b739ff9631588556
TinkeringAlways <alwaystinkering@outlook.com>
2015-09-19 19:34:44 -0500

    No Luck on Playing with Cubes 3........ :(

---
+++ PlayingWithCube3/Cube.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PlayingWithCube3
+{
+    public class Cube
+    {
+        // Fields
+        public int Side;
+        public double Volume;
+        public double Surface;
+
+        // Constructors
+        public Cube() { }
+
+        public Cube(int s)
+        {
+            Side = Math.Abs(s);
+            Volume = Math.Pow(s, 3);
+            Surface = (s * 2) * 6;
+        }
+
+        //// Property Setters
+        //public int Side => side;
+
+        //public double Volume => volume;
+
+        //public double Surface => surface;
+    }
+}
