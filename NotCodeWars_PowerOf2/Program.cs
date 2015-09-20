c5902222836bc0b8d111d1fef2103d7080f77e42
TinkeringAlways <alwaystinkering@outlook.com>
2015-09-20 11:10:06 -0500

    BuildingSpheres and PowerOf2 Algorithms!!

---
+++ NotCodeWars_PowerOf2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotCodeWars_PowerOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerOfTwo(8));
+            Console.WriteLine(PowerOfTwo(456789));
+            Console.ReadLine();
+        }
+
+        public static bool PowerOfTwo(ulong p)
+        {
+            return (p != 0) && ((p & (p - 1)) == 0);
+        }
+    }
+}
